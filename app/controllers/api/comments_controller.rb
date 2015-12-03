9e8769b1c9af555370bcd2d09766cc86711dfe75
y4ashida <y4ashida@gmail.com>
2015-12-04 02:09:13 +0900

    コメントを保存できるように

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -6,5 +6,15 @@ module Api
       render json: @comments
     end
 
+    def create
+      @comment = Comment.new(comment_params)
+      @comment.save
+    end
+
+  private
+    def comment_params
+      params.permit(:author, :text)
+    end
+
   end
 end
